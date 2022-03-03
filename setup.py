899e34717ac0d0c8e15eb17cb43adc810f7b7a24
staumoepeau <sione.taumoepeau@gmail.com>
2022-03-04 05:35:57 +1300

    Fix Setup Error

--- setup.py
+++ setup.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from setuptools import setup, find_packages
 
 with open("requirements.txt") as f:
@@ -13,23 +12,6 @@ setup(
 	description="App for TTI",
 	author="Sione Taumoepeau",
 	author_email="sione.taumoepeau@gmail.com",
-=======
-# -*- coding: utf-8 -*-
-from setuptools import setup, find_packages
-
-with open('requirements.txt') as f:
-	install_requires = f.read().strip().split('\n')
-
-# get version from __version__ variable in fwc_edu/__init__.py
-from fwc_edu import __version__ as version
-
-setup(
-	name='fwc_edu',
-	version=version,
-	description='Application to manage FWC Education Department',
-	author='Sione Taumoepeau',
-	author_email='sione.taumoepeau@gmail.com',
->>>>>>> 4c8ef093c7eb0eb4efece16aa09f7f910d1c0d68
 	packages=find_packages(),
 	zip_safe=False,
 	include_package_data=True,
