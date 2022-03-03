b84befca57d4b22891eaaa156881e1ab35285438
staumoepeau <sione.taumoepeau@gmail.com>
2022-03-04 04:59:06 +1300

    Update Student Info 04032022

--- fwc_edu/fwc_education/report/fwc_bank_remittance/fwc_bank_remittance.js
+++
@@ -1,9 +0,0 @@
-// Copyright (c) 2016, Sione Taumoepeau and contributors
-// For license information, please see license.txt
-/* eslint-disable */
-
-frappe.query_reports["FWC Bank Remittance"] = {
-	"filters": [
-
-	]
-};
