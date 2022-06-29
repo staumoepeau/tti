f58ae08e06a65dfbb4499ee32fcf98f3299c3fa8
staumoepeau <sione.taumoepeau@gmail.com>
2022-06-29 22:24:45 +1300

    Fix Program Enrollment 29 JUN 2022

--- tti/api.py
+++ tti/api.py
@@ -2,36 +2,35 @@
 # Copyright (c) 2021, Sione Taumoepeau and contributors
 # For license information, please see license.txt
 
-from __future__ import unicode_literals
-import math
-import frappe
-from frappe import _
-from frappe.utils import cstr
+#from __future__ import unicode_literals
+#import math
+#import frappe
+#from frappe import _
+#from frappe.utils import cstr
 
 
 #@frappe.whitelist(allow_guest=True)
-def get_permission_query_conditions_for_student(user):
-    if not user: user = frappe.session.user
+#def get_permission_query_conditions_for_student(user):
+#    if not user: user = frappe.session.user
     
-    if user == "Administrator":
-        return ""
-    else:
-        return """(`tabStudent`.user = '{user}')""" .format(user=user)
+#    if user == "Administrator":
+#        return ""
+#    else:
+#        return """(`tabStudent`.user = '{user}')""" .format(user=user)
 
 
-def has_permission(user):
-    if not user: user = frappe.session.user
-    if (user != "Administrator"):
+#def has_permission(user):
+#    if not user: user = frappe.session.user
+#    if (user != "Administrator"):
         # dont allow non Administrator user to view / edit Administrator user
-        return True
+#        return True
 
+#def get_permission_query_conditions_for_program_enrollment(user):
 
-def get_permission_query_conditions_for_program_enrollment(user):
-
-    if not user: user = frappe.session.user 
+#    if not user: user = frappe.session.user 
     
-    if user == "Administrator":
-        return ""
-    else:
-        user = frappe.db.get_value("Student",{"user": frappe.session.user},"name")
-        return """(`tabProgram Enrollment`.student = '{user}')""" .format(user=user)
+#    if user == "Administrator":
+#        return ""
+#    else:
+#        user = frappe.db.get_value("Student",{"user": frappe.session.user},"name")
+#        return """(`tabProgram Enrollment`.student = '{user}')""" .format(user=user)
